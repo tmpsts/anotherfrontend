f4017b2f838fc210567375f98428d07c384cac23
Matthew Denton <mwdenton@cs.olemiss.edu>
2024-12-28 14:52:29 -0500

    done #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
+import LogoTicker from "@/sections/LogoTicker";
+import Introduction from "@/sections/Introduction";
+import Features from "@/sections/Features";
+import Integrations from "@/sections/Integrations";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <LogoTicker />
+      <Introduction />
+      <Features />
+      <Integrations />
     </>
   );
 }
